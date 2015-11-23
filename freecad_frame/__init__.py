7c9e2f4f2fda1fcf4a778455ddea833cc576ca8b
looooo <sppedflyer@gmail.com>
2015-11-23 23:39:14 +0100

    setup.py : add icons

--- freecad_frame/__init__.py
+++ freecad_frame/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = "0.01"
+__version__ = "0.011"
 DIR = os.path.realpath(os.path.dirname(__file__)) + "/"
 ICON_PATH = DIR + "icons/"
 print(ICON_PATH)
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from freecad_dist import fc_install                 # custom freecad install
-from setuptools.core import setup
+from setuptools import setup
 import freecad_frame
 
 setup(cmdclass={'install': fc_install},
       install_requires=[],
       name='freecad_frame',
-      version=freecad_frame.version,
+      version=freecad_frame.__version__,
       description='some functionality for frame-building',
       url='my_website',
       author='looooo',
       license='LGPL2',
-      packages=["freecad_beam", "icons"],
+      packages=["freecad_frame", "freecad_frame/icons"],
       package_data = {"": ["*.svg"]})     # not std files (.py)
