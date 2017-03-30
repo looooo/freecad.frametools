e905a632f7118acdcdf6b0454a634a5581336ce5
looooo <sppedflyer@gmail.com>
2017-03-31 00:12:18 +0200

    add python2 support

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup
 setup(name='frame-tools',
       version='1.0',
-      packages=['freecad.modules.frame_tools'],
-      )
+      packages=['freecad', 'freecad.modules', 
+                'freecad.modules.frame_tools',
+                'freecad.modules.frame_tools.icons'])
