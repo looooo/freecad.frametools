f098e77d52878a292e6b1f6bb8476dec418bd299
looooo <sppedflyer@gmail.com>
2017-03-31 09:50:03 +0200

    use setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from distutils.core import setup
-setup(name='frame-tools',
-      version='1.0',
-      packages=['freecad', 'freecad.modules', 
-                'freecad.modules.frame_tools',
-                'freecad.modules.frame_tools.icons'])
+from setuptools import setup
+
+setup(name='freecad_frame',
+      version='0.0',
+      packages=['freecad',
+                'freecad.modules',
+                'freecad.modules.frame_tools'],
+      include_package_data=True)
