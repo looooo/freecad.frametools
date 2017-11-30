d5071e1b80bef8a4ee2d504d346561b1f79b4588
looooo <sppedflyer@gmail.com>
2017-11-30 10:18:48 +0100

    make it work

--- frame_tools/bspline_tools.py
+++ frame_tools/bspline_tools.py
@@ -4,37 +4,44 @@ import Part
 import FreeCADGui as Gui
 
 
-def curve_between_lines(p0, p1, v0, v1, n):
+def curve_between_lines(p0, p1, v0, v1, n=None):
+    if n is None:
+        n = np.cross(v0, v1)
+        n /= np.linalg.norm(n)
+    def project_to_plane(p):
+        return p - p.dot(n) * n
     tang0 = -v0 / np.linalg.norm(v0)
     tang1 = -v1 / np.linalg.norm(v1)
     knots = np.array([0., 1.])
     mults = np.array([4, 4])
-    nt = np.cross(tang0, tang1)
-    nt /= np.linalg.norm(nt)
-    s0, s1, _ = np.linalg.solve(np.array([tang0, - tang1, nt]), p1 - p0)
-    t_start = (abs(s0) + abs(s1)) / 3.
+    p0_proj, p1_proj, t0_proj, t1_proj = list(map(project_to_plane, [p0, p1, tang0, tang1]))
+    s0, s1 = abs(np.linalg.lstsq(np.array([t0_proj, t1_proj]).T, p1_proj - p0_proj)[0])
+    print(s0, s1)
     w0 = 0.56265951
-    def project_to_plane(p):
-        return p - n * (n.dot(p))
-    def min_squared_diff_curvature(params):
-        '''minimizing the squared difference to the mean curvature'''
-        t0 = params
-        weights = np.array([1, w0, w0, 1])
-        poles = [p0, p0 + t0 * tang0, p1 + t0 * tang1, p1]
-        poles = [project_to_plane(p) for p in poles]
+    t00 = 0.65863639
+    n0 = np.cross(n, t0_proj)
+    n1 = np.cross(n, t1_proj)
+    sn0 , sn1 = np.linalg.lstsq(np.array([n0, n1]).T, p1_proj - p0_proj)[0]
+    center = p0_proj + sn0 * n0
+    def min_function(params):
+        w0, t00 = params
+        t0 = t00 * abs(s0)
+        t1 = t00 * abs(s1)
+        weights = np.array([1., w0, w0, 1.])
+        poles = np.array([p0_proj, p0_proj + t0 * t0_proj,
+                          p1_proj + t1 * t1_proj, p1_proj])
+        r = (np.linalg.norm(center - p0_proj) + np.linalg.norm(center - p1_proj)) / 2.
         bs = Part.BSplineCurve()
         bs.buildFromPolesMultsKnots(poles, mults, knots)
         for i, wi in enumerate(weights):
             bs.setWeight(i + 1, wi)
-        edge = bs.toShape()
-        ti = np.linspace(edge.FirstParameter, edge.LastParameter, 100)
-        curvature = np.array([edge.curvatureAt(i) for i in ti])
-        curvature -= sum(curvature) / len(curvature) # - mean
-        return sum(curvature ** 2)
-    res = least_squares(min_squared_diff_curvature, [t_start])
-    t0 = res.x
+        dist = (np.array(bs.discretize(100)) - center) / r
+        return sum((np.linalg.norm(dist, axis=1) - 1)**2)
+    w0, t00 = least_squares(min_function, [w0, t00]).x
+    t0 = t00 * abs(s0)
+    t1 = t00 * abs(s1)
     weights = np.array([1, w0, w0, 1])
-    poles = np.array([p0, p0 + t0 * tang0, p1 + t0 * tang1, p1])
+    poles = np.array([p0, p0 + t0 * tang0, p1 + t1 * tang1, p1])
     bs = Part.BSplineCurve()
     bs.buildFromPolesMultsKnots(poles, mults, knots)
     for i, wi in enumerate(weights):
@@ -45,12 +52,18 @@ def curve_between_lines(p0, p1, v0, v1, n):
 def make_nurbs_connection():
     edge1 = Gui.Selection.getSelectionEx()[0].SubObjects[0]
     edge2 = Gui.Selection.getSelectionEx()[1].SubObjects[0]
-    normal = Gui.Selection.getSelectionEx()[2].SubObjects[0]
+    try:
+        normal = Gui.Selection.getSelectionEx()[2].SubObjects[0]
+    except Exception:
+        normal = None
     p11 = np.array(edge1.valueAt(edge1.FirstParameter))
     p12 = np.array(edge1.valueAt(edge1.LastParameter))
     p21 = np.array(edge2.valueAt(edge2.FirstParameter))
     p22 = np.array(edge2.valueAt(edge2.LastParameter))
-    n = np.array(normal.tangentAt(normal.FirstParameter))
+    if normal is None:
+        n = None
+    else:
+        n = np.array(normal.tangentAt(normal.FirstParameter))
     l = [np.linalg.norm(i) for i in [p11 - p21, p11 - p22, p12 - p21, p12 - p22]]
     l_min = min(l)
     i = l.index(l_min)
@@ -77,6 +90,10 @@ def make_nurbs_connection():
         s1 = -1
         t2 = edge2.LastParameter
         s2 = -1
+    if edge1.Orientation != 'Forward':
+        s1 *= -1
+    if edge2.Orientation != 'Forward':
+        s2 *= -1
 
     p1 = np.array(edge1.valueAt(t1))
     p2 = np.array(edge2.valueAt(t2))
